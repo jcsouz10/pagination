9179f2d0bb25fbd2328624737955c806a79a86c4
Jean Souza <jcsouz@br.ibm.com>
2018-04-24 15:40:48 -0300

    Changes at 15:40

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,7 +3,6 @@ import { getPeople } from './api';
 import OnScreen from './OnScreen';
 import ButtonsAndInput from './ButtonsAndInput';
 import './App.css'
-const axios = require ("axios");
 
 class App extends Component {
   constructor(props) {
@@ -29,14 +28,15 @@ class App extends Component {
       console.log(error)
     });
   }
-  
+
   filterNames = (input) => {
-    const filtered = this.state.peoples.filter(item => item.id.includes(input));
+    const filtered = this.state.peoples.filter(item => item.name.includes(input));
     const validate = filtered.length ? '' : 'Você vai adicionar um nome';
     this.setState({ filtered, currentPage: 0, input, validate });
   }
-  
+
   elementsOnScreen = () => {
+    console.log(this.state.input)
     const { currentPage, filtered } = this.state;
     return filtered
     .map((item) => <div className='item' key={item.id}> {item.name} <div> <button className='buttonDelete' onClick={() => this.remove(item.name, item.id)}> Delete </button> <button className='buttonAbout' onClick={() => this.about(item.country, item.age)}> About </button> </div> </div>)
@@ -46,19 +46,13 @@ class App extends Component {
   about = (coun, age) => {
     alert("• Country: "+coun +"\n• Age: "+age );
   }
-  
-  remove = (name,id) => {
-      axios.delete('http://127.0.0.1:9000/people/:'+id).
-      then((response=>this.state.filtered.splice(name,1)))
- }
-  
+
   nextPage = () => {
     console.log(this.state.filtered)
     const { currentPage, filtered } = this.state;
     if ((currentPage + 1) * this.state.elementsPerPage < filtered.length) {
       this.setState({ currentPage: this.state.currentPage + 1 });
     }
-    console.log(this.state.elementsPerPage)
   }
   
   previousPage = () => {
@@ -71,7 +65,6 @@ class App extends Component {
   addItem = () => {
     const inValue = { id: 99, name: this.state.input }
     this.setState({ filtered: this.state.peoples.concat(inValue), currentPage: 0, inputValue: '', validate: '' })
-    console.log(this.state.peoples)
   }
   
   
@@ -79,22 +72,15 @@ class App extends Component {
     this.setState({
       currentPage: 0, filtered: this.state.peoples
     })
-  }
-
-  handleChange=(event)=> {
-    const value = event.target.value;
-    this.setState({ elementsPerPage: value, currentPage: 0 });
-    console.log(this.state.newValueInput)
-  }
-  
+  }  
 
   render() {
     return (
       <div className="AllApp">
       <OnScreen validate={this.state.validate} onChangeElements={this.elementsOnScreen()} currentPage={this.state.currentPage} />
         <div className="menu">
-        <h1> Menu </h1>
-      <ButtonsAndInput hadle={this.handleChange.bind(this)} restorePage={this.restorePage} nextPage={this.nextPage} previousPage={this.previousPage} getValueInput={this.filterNames} addItem={this.addItem} />
+        <h1> Options </h1>
+      <ButtonsAndInput  restorePage={this.restorePage} nextPage={this.nextPage} previousPage={this.previousPage} getValueInput={this.filterNames} addItem={this.addItem} />
         </div>
       </div>
     );
--- myapp/src/ButtonsAndInput.js
+++ myapp/src/ButtonsAndInput.js
@@ -3,15 +3,10 @@ import './App.css'
 
 export default props => (
   <div className="ButtonsAndInput"  >
-    <input className='item2' onChange={(e) => props.getValueInput(e.target.value)}></input>
-    <button className='item2' onClick={props.previousPage}> Previous </button>
-    <button className='item2' onClick={props.nextPage}> Next </button>
-    <button className='item2' onClick={props.addItem}> Add </button>
-    <button className='item2' onClick={props.restorePage}> To restore page </button>
-    <select className='item2' onChange={props.handle}>
-      <option value="3" >3</option>
-      <option value="4" >4</option>
-      <option value="10" >10</option>
-    </select>
+    <input className='itemInput' onChange={(e) => props.getValueInput(e.target.value)}></input>
+    <button className='itemAdd' onClick={props.addItem}> Add </button>
+    <button className='itemPrevious' onClick={props.previousPage}> Previous </button>
+    <button className='itemNext' onClick={props.nextPage}> Next </button>
+    <button className='itemRestore' onClick={props.restorePage}> To restore page </button>
   </div>
 )
